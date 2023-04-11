5096ea8f3ef24a1e35f503217503b62102b55296
shekhar <shekharmeena48@gmail.com>
2023-04-11 16:11:10 +0530

    requirement.txt is updated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name = "Sensor",
+    version = "0.0.1",
+    author = "Shekhar",
+    author_email = "shekharmeena48@gmail.com",
+    packages = find_packages(),
+    install_requires = get_reqirements()
+)
